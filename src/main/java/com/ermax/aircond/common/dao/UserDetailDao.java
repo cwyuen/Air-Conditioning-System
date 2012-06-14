fe6f99a063ac4d6add15e572a1a804f873089ced
cwyuen <cwyuen1982@hotmail.com>
2012-06-14 21:26:08 +0800

    Testing

--- src/main/java/com/ermax/aircond/common/dao/UserDetailDao.java
+++ src/main/java/com/ermax/aircond/common/dao/UserDetailDao.java
@@ -1,9 +1,12 @@
 package com.ermax.aircond.common.dao;
 
+import java.util.List;
+
 import com.ermax.aircond.common.domain.UserDetail;
 import com.ermax.aircond.util.dao.AbstractHibernateDao;
 
 public interface UserDetailDao extends AbstractHibernateDao<UserDetail> {
 	
 	public UserDetail findAcUserByUsername(String username);
+	public List<UserDetail> findUserDetailByKeyword(String keyword);
 }
